87f9a391613f7a57b85e2adfd743943e094a6b19
Andres Breton <bretonics@users.noreply.github.com>
2016-09-09 12:27:16 -0400

    init email notifications backbone

---
+++ bin/Notification.pm
@@ -0,0 +1,101 @@
+package Notification;
+
+use Exporter qw(import);
+our @ISA = qw(Exporter);
+our @EXPORT = qw(email); #functions exported by default
+our @EXPORT_OK = qw(); #functions for explicit export
+
+use strict; use warnings; use diagnostics; use feature qw(say);
+use Carp;
+
+use MIME::Lite::TT::HTML;
+
+# ==============================================================================
+#
+#   CAPITAN:        Andres Breton, dev@andresbreton.com
+#   FILE:           Notification.pm
+#   LICENSE:
+#   USAGE:          Send email with attachments
+#   DEPENDENCIES:   - MIME::Lite::TT::HTML
+#
+# ==============================================================================
+
+=head1 NAME
+
+Notification - package
+
+=head1 SYNOPSIS
+
+Creation:
+    use Notification;
+
+=head1 DESCRIPTION
+
+
+=head1 EXPORTS
+
+=head2 Default Behaviors
+
+Exports email subroutine by default
+
+use Notification;
+
+=head2 Optional Behaviors
+
+Notification::;
+
+=head1 FUNCTIONS
+
+=cut
+
+=head2 email
+
+    Arg [1]     : Hash reference with email parameters
+
+    Example     : email(\%params)
+
+    Description : Sends email with attachments
+
+    Returntype  : NULL
+
+    Status      : Development
+
+=cut
+sub email {
+    my ($params) = @_;
+
+    my $options = {
+        'INCLUDE_PATH' => 'templates',
+    };
+
+    my $msg = MIME::Lite::TT::HTML->new(
+        From        => $params->{'from'},
+        To          => $params->{'to'},
+        Subject     => $params->{'subject'},
+        TimeZone    => 'America/Eastern',
+        Template    => {
+                            html => 'mail.html',
+                            text => 'mail.txt',
+                        },
+        Charset     => 'utf8',
+        TmplOptions => $options,    # reference
+        TmplParams  => $params,     # reference
+    );
+
+    # Set content type properly
+    $msg->attr("content-type"  => "multipart/mixed");
+
+    # Attach PDF to the message
+    $msg->attach(   Type        =>  'application/pdf',
+                    Path        =>  '/path/to/this-users.pdf',
+                    Filename    =>  'your-receipt.pdf',
+                    Disposition =>  'attachment'
+    );
+
+    $msg->send;
+    say "Email sent successfully";
+
+    return;
+}
+
+1;
